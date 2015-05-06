b94a64c0db3632d06c196aed3d584688b8280727
sullenor <iamhoneyboy@gmail.com>
2015-05-07 00:01:59 +0300

    Запуск плагинов

--- install.sh
+++ install.sh
@@ -6,16 +6,19 @@ cd `dirname $0`
 # Нужные переменные
 repository=`pwd`
 configs=${repository}/configs
+plugins=${repository}/plugins
 sources=${repository}/sources
 
 : ${USER:=sullenor}
 : ${EMAIL:=iamhoneyboy@gmail.com}
 
 
+# Создаем папку для локальной копии конфигов
 if [ ! -d $sources ]; then
   mkdir -p $sources
 fi
 
+# Копируем + Патчим конфиги
 for config in `ls -A $configs`; do
   if [ ! -f $sources/$config ]; then
     sed \
@@ -30,4 +33,9 @@ for config in `ls -A $configs`; do
     rm ~/$config
     ln -s $sources/$config ~/$config
   fi
+done
+
+# Запускаем плагины
+for plugin in `ls -A $plugins`; do
+  source $plugins/$plugin
 done
