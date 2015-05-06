f4582592945c05a0adc914cc1f220015f0238155
sullenor <iamhoneyboy@gmail.com>
2015-05-06 23:58:17 +0300

    Доработал плагин для oh-my-zsh

--- plugins/oh-my-zsh.sh
+++ plugins/oh-my-zsh.sh
@@ -4,3 +4,19 @@
 if [ ! -d ~/.oh-my-zsh ]; then
   curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 fi
+
+# Если нет файла, то облом :(
+if [ ! -f ~/.zshrc ]; then
+  exit
+fi
+
+# Если ссылка уже создана, то тоже ничего не делаем, так как нет оригинала
+if [ -h ~/.zshrc ]; then
+  exit
+fi
+
+# Создаем ссылку на файлик
+if [ $sources ]; then
+  mv ~/.zshrc $sources/.zshrc
+  ln -s $sources/.zshrc ~/.zshrc
+fi
