9bf26cbaef60e519362d35eb5200a7d8c1597a14
Alexey Litvinov <iamhoneyboy@gmail.com>
2015-05-10 14:16:26 +0300

    Поправил путь для линковки

--- plugins/oh-my-zsh/install.sh
+++ plugins/oh-my-zsh/install.sh
@@ -4,6 +4,9 @@ if [ ! -d ~/.oh-my-zsh ]; then
   curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 fi
 
+# На случай, если ZSH еще не существует
+: ${ZSH:=~/.oh-my-zsh}
+
 if [ -d $ZSH/custom ]; then
   stream $plugin_dir/mine.zsh > $SOURCES/mine.zsh
   ln -s $SOURCES/mine.zsh $ZSH/custom/mine.zsh
