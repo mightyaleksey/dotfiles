2aec9ff33241482fb069cef456cf3db23effc3d5
sullenor <iamhoneyboy@gmail.com>
2015-05-10 04:53:51 +0300

    Подробный скрипт для чистки дот-файлов

---
+++ tools/uninstall.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+cd `dirname $0`/../
+DOTFILES=`pwd`
+CONFIGS=${DOTFILES}/configs
+SOURCES=${DOTFILES}/sources
+
+echo 'Removing dotfiles from the home directory'
+for config in `ls -A $CONFIGS`; do
+  if [ -h ~/$config ] && [ "$SOURCES/$config" = "`readlink ~/$config`" ]; then
+    echo " - removing ~/$config"
+    rm ~/$config
+  fi
+done
+
+if [ -f ~/.oh-my-zsh/tools/uninstall.sh ]; then
+  echo 'Removing oh-my-zsh'
+  sh ~/.oh-my-zsh/tools/uninstall.sh
+fi
+
+if [ -d $SOURCES ]; then
+  echo 'Removing copy of the local dotfiles'
+  rm -rf $SOURCES
+fi
