47fde85e32301bb4b3724586bd7306013c8678fe
Alexey Litvinov <sullenor@yandex-team.ru>
2015-05-11 21:57:46 +0300

    Добавил несколько переменных для работы с дебиан пакетами

--- plugins/oh-my-zsh/mine.zsh
+++ plugins/oh-my-zsh/mine.zsh
@@ -1,2 +1,7 @@
 # Path to node.js modules
 export PATH=$PATH:node_modules/.bin
+
+# Helpers for the debian packages
+export DEBFULLNAME='#{user}'
+export DEBEMAIL='#{email}'
+export EMAIL='#{email}'
