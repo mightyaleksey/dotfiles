95fecb4b57ef9ab7b5e7713427486c00c811cb12
Alexey Litvinov <iamhoneyboy@gmail.com>
2016-11-14 13:22:44 +0300

    shortcut for node

--- .extention/oh-my-zsh/nodejs.plugin.zsh
+++ .extention/oh-my-zsh/nodejs.plugin.zsh
@@ -7,3 +7,18 @@ export PATH=~/.npm-global/bin:$PATH
 
 # Shortcut for node_modules/.bin folder
 export PATH=./node_modules/.bin:$PATH
+
+# Shortcut for `node index.js` and `node -p <command>` depending on args
+n() {
+  if [ "$#" -gt 0 ]; then
+    echo $1|sed -E "s/^([a-z\-]+)\./require(\"'\1'\")./"|xargs -I % node -p %
+    return $?
+  fi
+
+  if [ -f "index" ] || [ -f "index.js" ]; then
+    node index
+    return $?
+  fi
+
+  node
+}
