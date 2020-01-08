805f56f2724c969c1b2e0e9939a4bf840ce1074f
Aleksey Litvinov <iamhoneyboy@gmail.com>
2020-01-08 13:48:04 +0300

    the new vision

---
+++ readme.md
@@ -0,0 +1,15 @@
+# dotfiles
+My environment configuration.
+
+## prerequisite
+install [homebrew](https://brew.sh)
+```
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+```
+
+install [fish](https://fishshell.com)
+```
+brew install fish
+sudo echo /usr/local/bin/fish >> /etc/shells
+chsh -s /usr/local/bin/fish
+```
---
+++ scripts/setup.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+_dirname=$(cd $(dirname "$0") && pwd)
+
+ask()
+{
+    response=y
+
+    if [ -e "$HOME/$1" ]; then
+	echo "overwrite $1? \c"
+	read response
+    fi
+
+    if [ "$response" != "y" ] && [ "$response" != "n" ]; then
+	response=n
+    fi
+
+    if [ "$response" = "y" ]; then
+	($2 $1)
+    fi
+}
+
+to_copy_home()
+{
+    cp "$1" "$HOME/"
+}
+
+to_install_emacs()
+{
+    git clone --depth=1 git@github.com:sullenor/emacs.d.git "$HOME/$1"
+}
+
+echo "setup dotfiles"
+#ask .editorconfig to_copy_home
+#ask .gitconfig to_copy_home
+#ask .gitignore_global to_copy_home
+#ask .zshrc to_copy_home
+ask .emacs.d to_install_emacs
